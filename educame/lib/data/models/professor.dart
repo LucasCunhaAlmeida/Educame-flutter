a5fe64904af1bd9ae90550b10448543a6fc6e0db
Gabr1elFraga <gabrielsilveirafraga@hotmail.com>
2026-07-17 14:27:17 -0300

    feat(professor): implementa o modelo agregado Professor

--- educame/lib/data/models/professor.dart
+++ educame/lib/data/models/professor.dart
@@ -1,21 +1,39 @@
+import 'disciplina.dart';
+import 'pessoa.dart';
+
 class Professor {
   final int? id;
-  final int pessoaId;
+  final Pessoa pessoa;
   final String? bio;
   final String? diploma;
   final double valorHoraAula;
   final bool ativo;
+  final List<Disciplina> especialidades;
+  final double mediaAvaliacoes;
+  final int totalAvaliacoes;
 
-  Professor({
+  const Professor({
     this.id,
-    required this.pessoaId,
+    required this.pessoa,
     this.bio,
     this.diploma,
     required this.valorHoraAula,
     this.ativo = true,
+    this.especialidades = const [],
+    this.mediaAvaliacoes = 0,
+    this.totalAvaliacoes = 0,
   });
 
-  Map<String, dynamic> toMap() {
+  int get pessoaId => pessoa.id!;
+  String get nomeCompleto => pessoa.nomeCompleto;
+  String get iniciais => pessoa.iniciais;
+  bool get possuiAvaliacoes => totalAvaliacoes > 0;
+
+  String get especialidadesTexto {
+    return especialidades.map((disciplina) => disciplina.nome).join(', ');
+  }
+
+  Map<String, Object?> toMap() {
     return {
       'id': id,
       'pessoa_id': pessoaId,
@@ -26,14 +44,39 @@ class Professor {
     };
   }
 
-  factory Professor.fromMap(Map<String, dynamic> map) {
+  factory Professor.fromMap(
+    Map<String, Object?> map, {
+    required Pessoa pessoa,
+    List<Disciplina> especialidades = const [],
+  }) {
     return Professor(
-      id: map['id'],
-      pessoaId: map['pessoa_id'],
-      bio: map['bio'],
-      diploma: map['diploma'],
-      valorHoraAula: map['valor_hora_aula'],
+      id: map['id'] as int?,
+      pessoa: pessoa,
+      bio: map['bio'] as String?,
+      diploma: map['diploma'] as String?,
+      valorHoraAula: (map['valor_hora_aula'] as num).toDouble(),
       ativo: map['ativo'] == 1,
+      especialidades: List.unmodifiable(especialidades),
+      mediaAvaliacoes: (map['media_avaliacoes'] as num?)?.toDouble() ?? 0,
+      totalAvaliacoes: (map['total_avaliacoes'] as num?)?.toInt() ?? 0,
+    );
+  }
+
+  Professor copyWith({
+    List<Disciplina>? especialidades,
+    double? mediaAvaliacoes,
+    int? totalAvaliacoes,
+  }) {
+    return Professor(
+      id: id,
+      pessoa: pessoa,
+      bio: bio,
+      diploma: diploma,
+      valorHoraAula: valorHoraAula,
+      ativo: ativo,
+      especialidades: especialidades ?? this.especialidades,
+      mediaAvaliacoes: mediaAvaliacoes ?? this.mediaAvaliacoes,
+      totalAvaliacoes: totalAvaliacoes ?? this.totalAvaliacoes,
     );
   }
 }
